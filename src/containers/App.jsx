f0988013ac50720da83f84f4359a0d64436d6103
Kangho Lee <cannalee90@gmail.com>
2018-07-20 19:19:20 +0900

    first commit

---
+++ src/containers/App.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Player from './Player';
+import Upload from './Upload';
+import '../style/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">      
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <Switch>
+          <Route exact path='/' component={Player} />
+          <Route exact path='/upload' component={Upload} />
+
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/containers/Upload.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Player extends Component {
+  render() {
+    return (
+      <div className="Player">
+        <h1>Hello Upload</h1>
+      </div>
+    );
+  }
+}
+
+export default Player;
