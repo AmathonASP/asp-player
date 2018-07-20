2ca659fc620916245793e0086d014906cc004846
Kangho Lee <cannalee90@gmail.com>
2018-07-21 01:21:00 +0900

    upload page ui 구현 without dropzone

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -3,18 +3,20 @@ import { Switch, Route } from 'react-router-dom';
 import Player from './Player';
 import Upload from './Upload';
 import '../style/App.css';
+import '../style/Player.css';
+import '../style/Upload.css';
+import '../style/default.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">      
-        <header className="App-header">
+        {/* <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
-        </header>
+        </header> */}
         <Switch>
           <Route exact path='/' component={Player} />
           <Route exact path='/upload' component={Upload} />
-
         </Switch>
       </div>
     );
